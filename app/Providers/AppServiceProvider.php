098a5704ac6ecb7e24d82aa85015262efa5043d4
CrisViera <cristianvieracruz@gmail.com>
2025-08-24 22:22:17 +0100

    Modificar el limiter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,14 +12,12 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        // Limiter usado por 'throttle:api'
-        RateLimiter::for('api', function (Request $request) {
-            return $request->user()
-                ? Limit::perMinute(60)->by($request->user()->id)
-                : Limit::perMinute(30)->by($request->ip());
+        RateLimiter::for('register', function (Request $request) {
+            return [
+                Limit::perMinute(5)->by($request->ip()),
+            ];
         });
 
-        // Limiter específico para /login (opcional pero recomendado)
         RateLimiter::for('login', function (Request $request) {
             $key = 'login|'.$request->ip().'|'.(string) $request->input('email');
             return [ Limit::perMinute(5)->by($key) ];
